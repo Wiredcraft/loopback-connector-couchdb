a6b5b9ffe5de0d5184254b888a9dbca4941442b0
Makara Wang <makara15@gmail.com>
2016-05-25 17:26:16 +0800

    Use `merge` so `autoupdate` keeps user created design docs.

--- test/20.operation.test.js
+++ test/20.operation.test.js
@@ -79,6 +79,14 @@ describe('CouchDB operations', function() {
   });
 
   it('can use the DB', function(done) {
+    Person.create(persons[1]).then(function(person) {
+      person.id.should.equal('1');
+      person.name.should.equal('Mary');
+      done();
+    }).catch(done);
+  });
+
+  it('can use the DB', function(done) {
     Person.create(persons[3]).then(function(person) {
       person.id.should.be.String();
       person.name.should.equal('Jason');
@@ -107,6 +115,101 @@ describe('CouchDB operations', function() {
     connector.connect().call('viewAsync', 'find', 'byName', { keys: ['Jason'] }).then(function(res) {
       res.should.be.Object();
       res.should.have.property('rows').which.is.Array().with.length(1);
+      done();
+    }).catch(done);
+  });
+
+  it('can have another view', function(done) {
+    connector.connect().call('insertAsync', {
+      _id: '_design/group',
+      views: {
+        byAge: {
+          map: 'function(doc) { if (doc.age) emit(doc.age, null); }'
+        }
+      }
+    }).asCallback(done);
+  });
+
+  it('can use the other view', function(done) {
+    connector.connect().call('viewAsync', 'group', 'byAge', { keys: [24] }).then(function(res) {
+      res.should.be.Object();
+      res.should.have.property('rows').which.is.Array().with.length(2);
+      done();
+    }).catch(done);
+  });
+
+  it('can do autoupdate again', function(done) {
+    ds.autoupdate(function(err, res) {
+      if (err) {
+        return done(err);
+      }
+      done();
+    });
+  });
+
+  it('can use the view', function(done) {
+    connector.connect().call('viewAsync', 'find', 'byName', { keys: ['Charlie'] }).then(function(res) {
+      res.should.be.Object();
+      res.should.have.property('rows').which.is.Array().with.length(1);
+      done();
+    }).catch(done);
+  });
+
+  it('can still use the other view', function(done) {
+    connector.connect().call('viewAsync', 'group', 'byAge', { keys: [24] }).then(function(res) {
+      res.should.be.Object();
+      res.should.have.property('rows').which.is.Array().with.length(2);
+      done();
+    }).catch(done);
+  });
+
+  it('can remove the design docs', function(done) {
+    var connection = connector.connect();
+    return connection.call('getAsync', '_design/find').then(function(data) {
+      return connection.call('destroyAsync', data._id, data._rev);
+    }).asCallback(done);
+  });
+
+  it('can have another view', function(done) {
+    connector.connect().call('insertAsync', {
+      _id: '_design/find',
+      views: {
+        byAge: {
+          map: 'function(doc) { if (doc.age) emit(doc.age, null); }'
+        }
+      }
+    }).asCallback(done);
+  });
+
+  it('can use the other view', function(done) {
+    connector.connect().call('viewAsync', 'find', 'byAge', { keys: [24] }).then(function(res) {
+      res.should.be.Object();
+      res.should.have.property('rows').which.is.Array().with.length(2);
+      done();
+    }).catch(done);
+  });
+
+  it('can do autoupdate again', function(done) {
+    ds.autoupdate(function(err, res) {
+      if (err) {
+        return done(err);
+      }
+      done();
+    });
+  });
+
+  it('can use the view', function(done) {
+    connector.connect().call('viewAsync', 'find', 'byName', { keys: ['Charlie'] }).then(function(res) {
+      res.should.be.Object();
+      res.should.have.property('rows').which.is.Array().with.length(1);
+      done();
+    }).catch(done);
+  });
+
+  it('can still use the other view', function(done) {
+    connector.connect().call('viewAsync', 'find', 'byAge', { keys: [24] }).then(function(res) {
+      res.should.be.Object();
+      res.should.have.property('rows').which.is.Array().with.length(2);
       done();
     }).catch(done);
   });
